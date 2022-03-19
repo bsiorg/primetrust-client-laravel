1309a5d6232a3fe1cbd1342424ada7f5900b71b4
rizimore <rizimore@gmail.com>
2022-03-20 03:30:20 +0500

    init

--- src/PrimeTrustServiceProvider.php
+++ src/PrimeTrustServiceProvider.php
@@ -6,19 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class PrimeTrustServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->app->bind('primetrust', function ($app) {
+            return new PrimeTrust();
+        });
+    }
+
     public function boot()
     {
         $this->publishes([
             __DIR__ . '/../config/primetrust.php' => config_path('primetrust.php')
         ]);
-    }
 
-    public function register()
-    {
         $this->mergeConfigFrom(__DIR__ . '/../config/primetrust.php', 'primetrust');
-
-        $this->app->bind('primetrust', function () {
-            return new PrimeTrust();
-        });
     }
 }

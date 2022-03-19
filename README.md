81cf2435ff55bf2bb9e25f7924ff4cdb38db935a
rizimore <rizimore@gmail.com>
2022-03-20 02:59:32 +0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# PrimeTrust Client for Laravel
+A api client implementation in laravel of [primetrust](https://primetrust.com) api.
+
+## Author
+Mubashir Rasool Razvi (@rizimore)  
+rizimore@outlook.com
---
+++ src/PrimeTrust.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BsiOrg\PrimeTrust;
+
+class PrimeTrust
+{
+    protected $url;
+    protected $user;
+    protected $pass;
+
+    function __construct()
+    {
+        $this->url = config('primetrust.url');
+        $this->user = config('primetrust.user');
+        $this->pass = config('primetrust.pass');
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+    }
+}
